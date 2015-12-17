9be33c4d2b10b7bdc33069173e76a404d4a470ae
AUEzzat <amr.m.ezzat@gmail.com>
2015-12-17 04:27:32 +0200

    added jr instruction support

--- alu_control_unit.v
+++ alu_control_unit.v
@@ -1,24 +1,24 @@
-module alu_control_unit(Instruction,CUIn,ALUOut);
-input [5:0] Instruction;
-input [5:0] CUIn;
-output reg [3:0] ALUOut;
-always @ (CUIn) begin
-	case(CUIn)//opcode
+module alu_control_unit(Opcode,Funct,Output);
+input [5:0] Opcode;//operation control code / [31-26] instruction
+input [5:0] Funct;//R-type function /[5-0] instruction
+output reg [3:0] Output;
+always @ (Opcode) begin
+	case(Opcode)
 		6'b000_000:
-		case(Instruction)//R-type function/[5-0] instruction
-		6'b100_000:ALUOut=4'b0010;//add
-		6'b000_000:ALUOut=4'b0100;//sll
-		6'b100_100:ALUOut=4'b0000;//and
-		6'b100_111:ALUOut=4'b1100;//nor
-		6'b001_000:ALUOut=4'b1111;//jr
-		6'b101_010:ALUOut=4'b0111;//slt
+		case(Funct)
+		6'b100_000:Output=4'b0010;//add
+		6'b000_000:Output=4'b0100;//sll
+		6'b100_100:Output=4'b0000;//and
+		6'b100_111:Output=4'b1100;//nor
+		6'b001_000:Output=4'b1111;//jr
+		6'b101_010:Output=4'b0111;//slt
 		endcase
-		6'b100_011:ALUOut=4'b1000;//lw
-		6'b101_011:ALUOut=4'b1001;//sw
-		6'b001_000:ALUOut=4'b0011;//addi
-		6'b001_100:ALUOut=4'b0001;//andi
-		6'b000_100:ALUOut=4'b1010;//beq	
-		6'b000_011:ALUOut=4'b1011;//jal
+		6'b100_011:Output=4'b1000;//lw
+		6'b101_011:Output=4'b1001;//sw
+		6'b001_000:Output=4'b0011;//addi
+		6'b001_100:Output=4'b0001;//andi
+		6'b000_100:Output=4'b1010;//beq	
+		6'b000_011:Output=4'b1011;//jal
 	endcase
 end
 endmodule
--- control_unit.v
+++ control_unit.v
@@ -1,5 +1,6 @@
-module control_unit(Instruction,RegDst,Jump,Branch,MemRead,MemtoReg,ALUOp,MemWrite,ALUSrc,RegWrite,JumpRegister);
-input [5:0] Instruction;
+module control_unit(Opcode,Funct,RegDst,Jump,Branch,MemRead,MemtoReg,ALUOp,MemWrite,ALUSrc,RegWrite,JumpRegister);
+input [5:0] Opcode;//operation control code / [31-26] instruction
+input [5:0] Funct;//R-type function /[5-0] instruction
 output reg [2:0] RegDst;//Register file register(rt/rd) mux/selector to write data to
 output reg Jump;//jump to address or normal increment mux
 output reg Branch;
@@ -10,92 +11,114 @@ output reg MemWrite;
 output reg ALUSrc;//ALU 2nd input R-type or I-type mux/selector
 output reg RegWrite;
 output reg JumpRegister;//Jump to address specified in register 
-always @ (Instruction) begin
-	case(Instruction)//opcode
-		6'b000_000:begin//R-type instructions
-		RegDst<=2'b01;
+
+
+always @ (Opcode) begin
+	case(Opcode)
+		6'b000_000:begin//	R-type instructions	//////////////////
+		RegDst<=2'b01;							//
 		Jump<=1'b0;
-		Branch<=1'b0;
+		Branch<=1'b0;							//
 		MemRead<=1'b0;
-		MemtoReg<=2'b00;
-		ALUOp<=6'b000_000;
-		MemWrite<=1'b0;
+		MemtoReg<=2'b00;						//
+		ALUOp<=6'b000_000;           
+		MemWrite<=1'b0;							//
 		ALUSrc<=1'b0;
-		RegWrite<=1'b1;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
+		if (Funct==6'b001_000) begin//	jr instruction check		//
+		RegWrite<=1'b0;							//	
+		JumpRegister<=1'b1;						
+		end else							//
+		RegWrite<=1'b1;	
+		JumpRegister<=1'b0;						//
 		end
-		6'b100_011:begin//lw
+		//////////////////////////////////////////////////////////////////
+
+		6'b100_011:begin//	load word (lw)		//////////////////
 		RegDst<=2'b00;
-		Jump<=1'b0;
+		Jump<=1'b0;							//
 		Branch<=1'b0;
-		MemRead<=1'b1;
+		MemRead<=1'b1;							//
 		MemtoReg<=2'b01;
-		ALUOp<=6'b100_011;
+		ALUOp<=6'b100_011;						//
 		MemWrite<=1'b0;
-		ALUSrc<=1'b1;
+		ALUSrc<=1'b1;							//
 		RegWrite<=1'b1;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
 		end
-		6'b101_011:begin//sw
+		//////////////////////////////////////////////////////////////////
+
+		6'b101_011:begin//	save word (sw)		//////////////////
 		RegDst<=2'bxx;
-		Jump<=1'b0;
+		Jump<=1'b0;							//
 		Branch<=1'b0;
-		MemRead<=1'b0;
+		MemRead<=1'b0;							//
 		MemtoReg<=2'bxx;
-		ALUOp<=6'b101_011;
+		ALUOp<=6'b101_011;						//
 		MemWrite<=1'b1;
-		ALUSrc<=1'b1;
+		ALUSrc<=1'b1;							//
 		RegWrite<=1'b0;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
 		end
-		6'b001_000:begin//addi
+		//////////////////////////////////////////////////////////////////
+
+		6'b001_000:begin//	add imediate (addi)	//////////////////
 		RegDst<=2'b01;
-		Jump<=1'b0;
+		Jump<=1'b0;							//
 		Branch<=1'b0;
-		MemRead<=1'b0;
+		MemRead<=1'b0;							//
 		MemtoReg<=2'b00;
-		ALUOp<=6'b001_000;
-		MemWrite<=1'b0;
-		ALUSrc<=1'b1;
+		ALUOp<=6'b001_000;						//
+		MemWrite<=1'b0;	
+		ALUSrc<=1'b1;							//
 		RegWrite<=1'b1;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
 		end
-		6'b001_100:begin//andi
+		//////////////////////////////////////////////////////////////////
+
+		6'b001_100:begin//	and imediate(andi)	//////////////////
 		RegDst<=2'b01;
-		Jump<=1'b0;
+		Jump<=1'b0;							//
 		Branch<=1'b0;
-		MemRead<=1'b0;
+		MemRead<=1'b0;							//
 		MemtoReg<=2'b00;
-		ALUOp<=6'b001_100;
+		ALUOp<=6'b001_100;						//
 		MemWrite<=1'bx;
-		ALUSrc<=1'b1;
+		ALUSrc<=1'b1;							//
 		RegWrite<=1'b1;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
 		end
-		6'b000_100:begin//beq
+		//////////////////////////////////////////////////////////////////
+
+		6'b000_100:begin//	branch equal (beq)	//////////////////
 		RegDst<=2'bxx;
-		Jump<=1'b0;
+		Jump<=1'b0;							//
 		Branch<=1'b1;
-		MemRead<=1'b0;
+		MemRead<=1'b0;							//
 		MemtoReg<=2'bxx;
-		ALUOp<=6'b000_100;
+		ALUOp<=6'b000_100;						//
 		MemWrite<=1'b0;
-		ALUSrc<=1'b0;
+		ALUSrc<=1'b0;							//
 		RegWrite<=1'b0;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
 		end
-		6'b000_011:begin//jal
+		//////////////////////////////////////////////////////////////////
+
+		6'b000_011:begin// 	jump and link (jal)	//////////////////
 		RegDst<=1'b10;
-		Jump<=1'b1;
+		Jump<=1'b1;							//
 		Branch<=1'bx;
-		MemRead<=1'b1;
+		MemRead<=1'b1;							//
 		MemtoReg<=2'b10;
-		ALUOp<=6'b000_011;
+		ALUOp<=6'b000_011;						//
 		MemWrite<=1'b0;
-		ALUSrc<=1'b1;
+		ALUSrc<=1'b1;							//
 		RegWrite<=1'b0;
-		JumpRegister<=1'b0;
+		JumpRegister<=1'b0;						//
 		end
+		//////////////////////////////////////////////////////////////////
+
 	endcase
+
 end
 endmodule
